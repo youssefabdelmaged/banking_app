3a55f2e288e07cc1f6cc3f48f360d50327f6af16
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-12-01 19:48:38 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import "../globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      sidebar
+      {children}
+    </main>
+  );
+}
