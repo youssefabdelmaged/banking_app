2bd988b3a440a7557552bc7cd8760d4d90e5ee6f
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2025-02-22 18:27:08 +0200

    implement auth, appwrite, and sentry

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,6 +12,12 @@ export const signIn = async ({ email, password }: signInProps) => {
 
     const response = await account.createEmailPasswordSession(email, password);
 
+    cookies().set("appwrite-session", response.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
     return parseStringify(response);
   } catch (error) {
     console.error("Error", error);
